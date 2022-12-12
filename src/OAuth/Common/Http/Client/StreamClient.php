f8953970236bc981e5d1124c3aa51416051f653c
Lucas de Rijke <lucas.de.rijke@gmail.com>
2022-12-12 16:17:56 +0100

    Removed unused files and added curl to streamclient

--- src/OAuth/Common/Http/Client/StreamClient.php
+++ src/OAuth/Common/Http/Client/StreamClient.php
@@ -2,12 +2,11 @@
 
 namespace OAuth\Common\Http\Client;
 
-use InvalidArgumentException;
 use OAuth\Common\Http\Exception\TokenResponseException;
 use OAuth\Common\Http\Uri\UriInterface;
 
 /**
- * Client implementation for streams/file_get_contents.
+ * Client implementation for streams/file_get_contents
  */
 class StreamClient extends AbstractClient
 {
@@ -15,58 +14,67 @@ class StreamClient extends AbstractClient
      * Any implementing HTTP providers should send a request to the provided endpoint with the parameters.
      * They should return, in string form, the response body and throw an exception on error.
      *
+     * @param UriInterface $endpoint
      * @param mixed        $requestBody
+     * @param array        $extraHeaders
      * @param string       $method
      *
      * @return string
+     *
+     * @throws TokenResponseException
+     * @throws \InvalidArgumentException
      */
     public function retrieveResponse(
         UriInterface $endpoint,
         $requestBody,
-        array $extraHeaders = [],
+        array $extraHeaders = array(),
         $method = 'POST'
     ) {
         // Normalize method name
         $method = strtoupper($method);
 
-        $extraHeaders = $this->normalizeHeaders($extraHeaders);
+        $this->normalizeHeaders($extraHeaders);
 
         if ($method === 'GET' && !empty($requestBody)) {
-            throw new InvalidArgumentException('No body expected for "GET" request.');
+            throw new \InvalidArgumentException('No body expected for "GET" request.');
         }
 
         if (!isset($extraHeaders['Content-Type']) && $method === 'POST' && is_array($requestBody)) {
             $extraHeaders['Content-Type'] = 'Content-Type: application/x-www-form-urlencoded';
         }
 
-        $host = 'Host: ' . $endpoint->getHost();
+        $host = 'Host: '.$endpoint->getHost();
         // Append port to Host if it has been specified
         if ($endpoint->hasExplicitPortSpecified()) {
-            $host .= ':' . $endpoint->getPort();
+            $host .= ':'.$endpoint->getPort();
         }
 
-        $extraHeaders['Host'] = $host;
+        $extraHeaders['Host']       = $host;
         $extraHeaders['Connection'] = 'Connection: close';
 
         if (is_array($requestBody)) {
             $requestBody = http_build_query($requestBody, '', '&');
         }
-        $extraHeaders['Content-length'] = 'Content-length: ' . strlen($requestBody);
+        $extraHeaders['Content-length'] = 'Content-length: '.strlen($requestBody);
 
-        $context = $this->generateStreamContext($requestBody, $extraHeaders, $method);
+        //$context = array($requestBody, $extraHeaders, $method);
+        $context = $extraHeaders;
+        //$context = $this->generateStreamContext($requestBody, $extraHeaders, $method);
 
         $level = error_reporting(0);
-        $response = file_get_contents($endpoint->getAbsoluteUri(), false, $context);
+        $response = $this->file_get_contents_curl($endpoint->getAbsoluteUri(), $context);
+        //$response = file_get_contents($endpoint->getAbsoluteUri(), false, $context);
+        
+        var_dump($response);
         error_reporting($level);
         if (false === $response) {
             $lastError = error_get_last();
-            if (null === $lastError) {
+            if (is_null($lastError)) {
                 throw new TokenResponseException(
                     'Failed to request resource. HTTP Code: ' .
-                    ((isset($http_response_header[0])) ? $http_response_header[0] : 'No response')
+                    ((isset($http_response_header[0]))?$http_response_header[0]:'No response')
                 );
             }
-
             throw new TokenResponseException($lastError['message']);
         }
 
@@ -76,17 +84,55 @@ class StreamClient extends AbstractClient
     private function generateStreamContext($body, $headers, $method)
     {
         return stream_context_create(
-            [
-                'http' => [
-                    'method' => $method,
-                    'header' => implode("\r\n", array_values($headers)),
-                    'content' => $body,
+            array(
+                'http' => array(
+                    'method'           => $method,
+                    'header'           => implode("\r\n", array_values($headers)),
+                    'content'          => $body,
                     'protocol_version' => '1.1',
-                    'user_agent' => $this->userAgent,
-                    'max_redirects' => $this->maxRedirects,
-                    'timeout' => $this->timeout,
-                ],
-            ]
+                    'user_agent'       => $this->userAgent,
+                    'max_redirects'    => $this->maxRedirects,
+                    'timeout'          => $this->timeout
+                ),
+            )
         );
+    }
+
+        /** 
+     * Curl get contents instead of file_get_contents.
+     * 
+     * @param  string The URL to grab contents of
+     * @param  array  Post data to send to the url, empty if not desired
+     * @param  mixed  Bool false if no desired verbose output, string of filename to write to if verbose output desired
+     * @param  mixed  False if referer not desired, string if you wish to set a referrer url
+     * @param  mixed  False if no cookie desired, string to set the cookie filepath
+     * @param  bool   Whether or not to set the return transfer option on
+     * @return mixed  String of data retrieved from the url if successful, boolean false if not successful 
+     */
+    function file_get_contents_curl($url, $data) {
+
+        $curl = curl_init();
+    
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: ' . $data["Authorization"]       
+            ),
+        ));
+    
+        $response = curl_exec($curl);
+    
+        echo curl_error($curl) . '<br/>';
+    
+        curl_close($curl);
+    
+        return $response;
     }
 }
